93de66ea5b868983312e170094414b664e2ca739
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-10-26 14:03:02 -0400

    enable other window managers

--- shared/desktops.nix
+++ shared/desktops.nix
@@ -2,10 +2,10 @@
   desktops = {
     enable = true;
     sway.enable = true;
-    # awesome.enable = true;
-    # ratpoison.enable = true;
+    awesome.enable = true;
+    ratpoison.enable = true;
     qtile.enable = true;
-    # i3gaps.enable = true;
+    i3gaps.enable = true;
     gnome.enable = true;
     # plasma.enable = true;
   };
